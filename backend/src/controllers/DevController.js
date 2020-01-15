ceaf3bd9661e3f44ee2f3118af1b3817e494167f
João Nascimento <thecurrentuser@live.com>
2020-01-15 11:13:35 -0300

    Alteração no DevController para validar se já existe um Dev no banco

--- backend/src/controllers/DevController.js
+++ backend/src/controllers/DevController.js
@@ -7,26 +7,33 @@ module.exports = {
         //Corpo da requisição
         const { github_username, techs, latitude, longitude } = request.body;
         //yarn add axios
-        const apiResponse = await axios.get(`https://api.github.com/users/${github_username}`);
-    
-        //Cria 3 variáveis, e as preenche de acordo com a resposta do github.
-        const { name = login, avatar_url, bio } = apiResponse.data;
-    
-        const techsArray = techs.split(',').map(tech => tech.trim());
-    
-        const location = {
-            type: 'Point',
-            coordinates: [longitude, latitude],
+        //Antes de buscar na api do github, verificar se o dev já está cadastrado no mongoDB.
+        //Consulta
+        let dev = await Dev.findOne({ github_username });
+
+        if (!dev) {
+            const apiResponse = await axios.get(`https://api.github.com/users/${github_username}`);
+
+            //Cria 3 variáveis, e as preenche de acordo com a resposta do github.
+            const { name = login, avatar_url, bio } = apiResponse.data;
+
+            const techsArray = techs.split(',').map(tech => tech.trim());
+
+            const location = {
+                type: 'Point',
+                coordinates: [longitude, latitude],
+            };
+
+            dev = await Dev.create({
+                github_username,
+                name,
+                avatar_url,
+                bio,
+                techs: techsArray,
+                location,
+            });
         };
-    
-        const dev = await Dev.create({
-            github_username,
-            name,
-            avatar_url,
-            bio,
-            techs: techsArray,
-            location,
-        });
+
         return response.json(dev);
     }
 };
