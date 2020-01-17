8456f59c56699d235962092d353b31941b8016a5
João Nascimento <thecurrentuser@live.com>
2020-01-16 21:29:19 -0300

    Preparação dos estados para salvar as informações do formulário + pegar localização do navegador; correções

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -17,7 +17,7 @@
 		-> Chamar JS no HTML, entre chaves { }.
 		-> Container, no lugar de DIV, para não quebrar o estilo, utilizar <> </> "Fragment".
 */
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import './global.css';
 import './App.css';
@@ -25,39 +25,113 @@ import './Sidebar.css';
 import './Main.css';
 
 function App() {
+  //Manter acesso real ao que o usuário digitou no input
+
   const [counter, setCounter] = useState(0);
+  const [latitude, setLatitude] = useState('');
+  const [longitude, setLongitude] = useState('');
+  const [github_username, setGitHubUsername] = useState('');
+  const [techs, setTechs] = useState('');
+
 
   function increment() {
     setCounter(counter + 1);
+  }
+
+  /* 
+  O que eu vou fazer para controlar quando a função irá executar?
+
+	React oferece uma função chamada useEffect, ele serve para dispararmos uma função toda vez que uma informação alterar, ou uma única vez durante a renderizaçao do componente.
+	Ele recebe dois parâmetros; Qual função e quando.
+	useEffect(() => {} ,[]);
+  Se o vetor estiver vazio vai executar apenas uma vez.
+  --> Dependências do useEffect.
+	navigator.geolocation.getCurrentPosition();
+  */
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(
+      //Retorna a posição do usuário, se deu tudo certo.
+      (position) => {
+        const { latitude, longitude } = position.coords;
+        // React segue um padrão de programação imperativa.
+        // Criar um estado e o componente precisa saber se comportar com base nesse estado.
+        setLatitude(latitude);
+        setLongitude(longitude);
+      },
+      //Caso aconteça algum erro, chamar essa função.
+      (err) => {
+        console.log(err);
+      },
+      {
+        timeout: 30000,
+      }
+    )
+  }, []);
+
+  // Função que será disparada quando o usuário clicar no submit do formulário.
+  // EVENTO ---> e
+  // submit do formulário tem o comportamento padrão de enviar o usuário para outra tela.
+  // Evitar isso na função que "salva" o formulário
+  async function handleAddDev(e){
+    e.preventDefault();
+    // Fazer a primeira chamada a API para adicionar o DEV a listagem.
+    
   }
 
   return (
     <div id="app">
       <aside>
         <strong>Cadastrar</strong>
-        <form>
-          <div class="input-block">
+        <form onSubmit={handleAddDev}>
+          <div className="input-block">
             <label htmlFor="github_username">Usuário do Github</label>
-            <input name="github_username" id="github_username" required />
+            <input
+              name="github_username"
+              id="github_username"
+              value={github_username}
+              required
+              onChange={e => setGitHubUsername(e.target.value)}
+            />
           </div>
 
-          <div class="input-block">
+          <div className="input-block">
             <label htmlFor="techs">Tecnologias</label>
-            <input name="techs" id="techs" required />
+            <input
+              name="techs"
+              id="techs"
+              value={techs}
+              required
+              onChange={e => setTechs(e.target.value)}
+            />
           </div>
 
           <div className="input-group">
-            <div class="input-block">
+            <div className="input-block">
               <label htmlFor="latitude">Latitude</label>
-              <input name="latitude" id="latitude" required />
+              <input
+                type="number"
+                name="latitude"
+                id="latitude"
+                required
+                value={latitude}
+                /*Armazernar o valor de um input dentro de um valor estado*/
+                onChange={e => setLatitude(e.target.value)}
+              />
             </div>
 
-            <div class="input-block">
+            <div className="input-block">
               <label htmlFor="longitude">Longitude</label>
-              <input name="longitude" id="longitude" required />
+              <input
+                type="number"
+                name="longitude"
+                id="longitude"
+                required value={longitude}
+                onChange={e => setLongitude(e.target.value)}
+              />
             </div>
           </div>
-          
+
           <button type="submit">Salvar</button>
         </form>
       </aside>
